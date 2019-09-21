758b8dbd8eade4cd88d813c73437c6380cdfdf2c
Justinas Garipovas <justinas.garipovas@gmail.com>
2019-09-21 22:46:02 +0300

    Controller, Model, View basic initialization

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdministrationController extends AbstractController
@@ -10,10 +12,26 @@ class AdministrationController extends AbstractController
     /**
      * @Route("/", name="administration")
      */
-    public function index()
+    public function index(Request $request)
     {
+        dump($request);
+
         return $this->render('administration/index.html.twig', [
             'controller_name' => 'AdministrationController',
         ]);
     }
+
+    /**
+     * @Route("/new", name="add-new-client")
+     */
+    public function addNewVisitor(Request $request)
+    {
+        $clientName = $request->request->get("clientName");
+
+        //TODO: Validation of the name (validation class)
+
+
+        return new JsonResponse(['client'=>'wa']);
+    }
+
 }
